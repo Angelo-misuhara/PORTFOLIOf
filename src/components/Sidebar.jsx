142fc2c49d71c9f5a33522fbf3b23e8a79f05a60
ogel <yocorangelo18@gmail.com>
2024-01-17 19:01:30 +0800

    add colors to li

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,16 +6,16 @@ const Sidebar = () => {
 			<h1 className='text-3xl font-thin'>Yocor,Angelo T.</h1>
 			<div className='mt-[10rem]'>
 				<ul  className='flex flex-col gap-4 p-2 pt-3'>
-					<li className='text-[1.4rem] hover:text-[2.5rem]'>
+					<li className='text-[1.4rem] font-[#C8BAAB] hover:text-[2.5rem]'>
 					<a href="">	Main</a>
 					</li>
-						<li className='text-[1.4rem] hover:text-[2.5rem]'>
+						<li className='text-[1.4rem] font-[#C8BAAB] hover:text-[2.5rem]'>
 						<a href="">Projects</a>
 					</li>
-						<li className='text-[1.4rem] hover:text-[2.5rem]'>
+						<li className='text-[1.4rem] font-[#C8BAAB] hover:text-[2.5rem]'>
 						<a href="">Contact</a>
 					</li>
-						<li className='text-[1.4rem] hover:text-[2.5rem]'>
+						<li className='text-[1.4rem] font-[#C8BAAB] hover:text-[2.5rem]'>
 						<a href="">About</a>
 					</li>
 				</ul>
