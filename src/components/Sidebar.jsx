02efa84b4f1ca7c460db3229e41f7d9031c83563
ogel <yocorangelo18@gmail.com>
2024-01-19 15:48:00 +0800

    remove public path

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
 			
         <div className="lg:mb-[7rem] mb-5">
           <div>
-            <img src="/public/images/LOGO.png" className="w-[4rem]" alt="" />
+            <img src="/images/LOGO.png" className="w-[4rem]" alt="" />
           </div>
           {/* <div className="hamburger-menu mb-7">
     <input id="menu__toggle" type="checkbox" />
