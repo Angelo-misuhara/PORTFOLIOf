14059cabc3d69289d079413f3b88f41d6e7438e0
ogel <yocorangelo18@gmail.com>
2024-01-21 12:29:18 +0800

    added modal

--- src/layouts/ContactPage.jsx
+++ src/layouts/ContactPage.jsx
@@ -1,8 +1,9 @@
-import  { useRef } from 'react';
+import  { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const ContactPage = () => {
 
+  const [oke,setOke] = useState(false)
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -12,10 +13,14 @@ const ContactPage = () => {
       .then((result) => {
         console.log(result.text);
         form.current.reset();
+        setOke(true)
       }, (error) => {
         console.log(error.text);
       });
   };
+  const closeModal = () => {
+    setOke(false)
+  }
   return (
     <>
       <div className='h-screen flex flex-col  ' id='ContactPage'>
@@ -46,6 +51,7 @@ const ContactPage = () => {
             </div>
             <div className=' flex justify-end'>
               <button type="submit" className="text-white  bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/55 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Send</button>
+              <h1></h1>
             </div>
           </form>
           <div className=''>
@@ -55,6 +61,27 @@ const ContactPage = () => {
         </div>
         
       </div>
+      {oke && <div id="popup-modal" tabIndex="-1" className=" overflow-y-auto overflow-x-hidden flex fixed top-0  z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div className="relative p-4 w-full max-w-md max-h-full">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" onClick={closeModal} className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span className="sr-only">Close modal</span>
+            </button>
+            <div className="p-4 md:p-5 text-center">
+              <div className='flex justify-center'>
+                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+<path fill="#4caf50" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"></path><path fill="#ccff90" d="M34.602,14.602L21,28.199l-5.602-5.598l-2.797,2.797L21,33.801l16.398-16.402L34.602,14.602z"></path>
+</svg>
+              </div>
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Successfully send!</h3>
+              
+            </div>
+        </div>
+    </div>
+</div>}
      
     </>
   )
